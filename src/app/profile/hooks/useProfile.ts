1fe87488dc299f5b4e13b60ef1e1111612403a3e
Dylan <dylancc20@gmail.com>
2025-10-06 10:30:00 -0400

    feat: add button to open fixer profile modal

---
+++ src/app/profile/hooks/useProfile.ts
@@ -0,0 +1,44 @@
+import { useState, useEffect } from 'react';
+type ProfileData = {
+  name: string;
+  photo_url: string;
+  role: string;
+  ratings: {
+    1: number;
+    2: number;
+    3: number;
+  };
+  average_rating: number;
+};
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+export const useProfile = () => {
+  const [data, setData] = useState<ProfileData | null>(null);
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(`${BASE_URL}/profile`, {
+        //BASE_URL is http://localhost:3000/api in .env.local
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+      const result = await response.json();
+      const totalSum = result.ratings[1] + result.ratings[2] + result.ratings[3];
+      const rating = {
+        1: Math.trunc((result.ratings[1] / totalSum) * 100),
+        2: Math.trunc((result.ratings[2] / totalSum) * 100),
+        3: Math.trunc((result.ratings[3] / totalSum) * 100),
+      };
+      const averageRating: number = Number(
+        (totalSum === 0
+          ? 0
+          : (1 * result.ratings[1] + 2 * result.ratings[2] + 3 * result.ratings[3]) / totalSum
+        ).toFixed(1),
+      );
+
+      setData({ ...result, ratings: rating, average_rating: averageRating });
+    };
+    fetchData();
+  }, []);
+  return { data };
+};
---
+++ src/app/profile/page.tsx
@@ -0,0 +1,25 @@
+'use client';
+import FixerProfile from './fixer-profile-modal';
+import { useState } from 'react';
+export default function ProfilePage() {
+  const [isOpen, setIsOpen] = useState(false);
+  const handleShowProfile = () => {
+    setIsOpen(true);
+  };
+  const closeShowProfile = () => {
+    setIsOpen(false);
+  };
+  return (
+    <>
+      <div className="flex items-center justify-center min-h-screen bg-gray-100">
+        <button
+          onClick={handleShowProfile}
+          className="px-4 py-2 text-sm font-medium text-black border border-black bg-white rounded-lg hover:bg-slate-100 cursor-pointer  transition-colors"
+        >
+          Ver perfil
+        </button>
+      </div>
+      {isOpen && <FixerProfile isOpen={isOpen} onClose={closeShowProfile} />}
+    </>
+  );
+}
