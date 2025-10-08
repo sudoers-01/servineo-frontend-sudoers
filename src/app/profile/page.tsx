856ea5fbd3ab258c6aece9f5c514a840efca5a40
Dylan <dylancc20@gmail.com>
2025-10-08 18:23:19 -0400

    style: change double quotes to single quotes

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -11,10 +11,10 @@ export default function ProfilePage() {
   };
   return (
     <>
-      <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <div className='flex items-center justify-center min-h-screen bg-gray-100'>
         <button
           onClick={handleShowProfile}
-          className="px-4 py-2 text-sm font-medium text-black border border-black bg-white rounded-lg hover:bg-slate-100 cursor-pointer  transition-colors"
+          className='px-4 py-2 text-sm font-medium text-black border border-black bg-white rounded-lg hover:bg-slate-100 cursor-pointer  transition-colors'
         >
           Ver perfil
         </button>
