e5139d1c82cb0638bb27c82ab74375c72fbc4d20
LucianaBarrosoF <202200291@est.umss.edu>
2025-10-10 17:38:07 -0400

    feat(sign up): Añadir página de registro

---
+++ src/app/controlC/components/registro/page.tsx
@@ -0,0 +1,12 @@
+"use client"
+
+import GoogleLayout from "./googleLayout"
+import ContenedorRegistro from "./contenedorRegistro"
+
+export default function RegistroPage() {
+  return (
+    <GoogleLayout>
+      <ContenedorRegistro />
+    </GoogleLayout>
+  )
+}
