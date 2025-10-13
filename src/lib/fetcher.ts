cce74cb2a1d239fcced41ce71dfcc7d29c3ead1f
CHAVEZ <andresochavezr@gmail.com>
2025-10-12 20:47:20 -0400

    refactor(rq03-4): scope ratings utils; remove old root hooks/lib/mocks

--- src/lib/fetcher.ts
+++
@@ -1,6 +0,0 @@
-// src/lib/fetcher.ts
-export async function jsonFetcher<T = unknown>(input: RequestInfo, init?: RequestInit) {
-  const res = await fetch(input, init)
-  if (!res.ok) throw new Error('Error when checking grades')
-  return (await res.json()) as T
-}
