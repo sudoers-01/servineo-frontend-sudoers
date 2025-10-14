da66a1def99a30f8352ba8c3e8eaf483c30e3180
Paredes <202301667@est.umss.edu>
2025-10-14 18:40:54 -0400

    frontend funcional

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,13 +3,13 @@ import { loadStripe } from "@stripe/stripe-js";
 import { Elements } from "@stripe/react-stripe-js";
 import CardList from "../components/CardList";
 
-const stripePromise = loadStripe("pk_test_51RnsLKCLi8Bq7NWUnbwBTP7zvMrWlIIauNMx47smdfpFmh3dpgpmZ2MQnOwUGUezb6tnDcenLDQWocnfjH4Lavu900ZiE4qZtJ");
+const stripePromise = loadStripe("pk_test_51SHGq0Fp8K0s2pYx4l5z1fkIcXSouAknc9gUV6PpYKR8TjexmaC3OiJR9jNIa09e280Pa6jGVRA6ZNY7kSCCGcLt002CEmfDnU");
 
 export default function Home() {
-  const requesterId = "68ed47b64ed596d659c1ed92"; // usuario real
+  const requesterId = "68ed47b64ed596d659c1ed8f"; // usuario real
   const fixerId = "68ed473e4ed596d659c1ed8c"; // fixer real
   const jobId = "68ea51ee0d80087528ad803f"; // job real
-  const amount = 300; 
+  const amount = 33300; 
 
   return (
     <Elements stripe={stripePromise}>
