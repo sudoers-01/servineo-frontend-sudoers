1d8dd3cb5cc3a941683f62d59dfc8699a40ed58a
Ignacio Jaldin <ignaciojaldin72@gmail.com>
2025-10-29 23:42:28 -0400

    Desktop Calendar fixed ultimo

--- src/app/calendar/page.tsx
+++ src/app/calendar/page.tsx
@@ -1,25 +1,52 @@
 
 "use client";
+
+import { useState, useEffect } from "react";
 import DesktopCalendar from "@/components/calendar/DesktopCalendar";
 
+import MobileCalendar from "@/components/calendar/mobile/MobileCalendar";
+import MobileList from "@/components/list/MobileList";
 
 
 const fixer_id = "user_fixer_1234";
-export default function Test() {
+export default function CalendarPage() {
 
+
+    const [requesterId, setRequesterId] = useState("user_requester_5678");
+
+    const [selectedDate, setSelectedDate] = useState<Date>(new Date());
+    const today = new Date();
+
+    const handleDataChange = (newDate: Date) => {
+        setSelectedDate(newDate);
+    }
 
     return (
-        <div>
-            <div className="bg-white h-300">
+        <div className="flex flex-col bg-white min-h-screen">
+
+            <div className="flex justify-center md:block hidden">
                 <DesktopCalendar
                     fixer_id={fixer_id}
-                    requester_id={'unu'}
+                    requester_id={"ungu"}
+                />
+
+            </div>
+
+            <div className="flex flex-col md:hidden justify-center gap-4" >
+                <MobileCalendar
+                    fixer_id={fixer_id}
+                    selectedDate={selectedDate}
+                    onSelectDate={handleDataChange}
+
+                />
+                <div></div>
+                <MobileList
+                    selectedDate={selectedDate}
+                    fixerId={fixer_id}
+                    requesterId={requesterId}
+                    onDateChange={handleDataChange}
                 />
             </div>
-            <div className="m-30"></div>
-
-            <div className="m-30"></div>
-
         </div>
     );
 }
