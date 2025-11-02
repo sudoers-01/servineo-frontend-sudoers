f580882810d7dc931e5482522016437d78c78fb1
alex <202300124@est.umss.edu>
2025-11-01 20:42:55 -0400

    feat formType Additions

--- src/components/appointments/forms/modules/LocationSection.tsx
+++ src/components/appointments/forms/modules/LocationSection.tsx
@@ -1,19 +1,31 @@
+export type FormType = 'create'|'edit'|'view'
 interface LocationSectionProps {
   address: string;
   error?: string;
+  formtype: FormType;
   onOpenLocationModal: () => void;
 }
 
-export const LocationSection = ({ address, error, onOpenLocationModal }: LocationSectionProps) => {
+export const LocationSection = ({ address, error, onOpenLocationModal,formtype }: LocationSectionProps) => {
   return (
     <div className="flex flex-col gap-1">
       <div
         onClick={onOpenLocationModal}
         className="text-center py-3 border border-gray-300 rounded-lg bg-gray-50 hover:bg-gray-100 cursor-pointer"
       >
-        <p className="text-sm font-medium text-gray-700">
+        {
+          formtype==='view' ? (
+            <p className="text-sm font-medium text-gray-700">
+                Ver Ubicacion
+            </p>
+          ) : null
+        }
+        {formtype !== 'view' && (
+          <p className="text-sm font-medium text-gray-700">
           📍 {address ? "Editar ubicación" : "Seleccionar ubicación"}
-        </p>
+          </p>
+        )}
+
         {error && <p className="text-red-600 text-sm mt-1">{error}</p>}
       </div>
 
