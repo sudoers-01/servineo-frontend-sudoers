6781952998c5de8eba8c5e3dc6e27135adbf6731
Ignacio Jaldin <ignaciojaldin72@gmail.com>
2025-11-07 13:07:44 -0400

    fix(calendar): fixed calendar bugs

--- src/app/calendar/page.tsx
+++ src/app/calendar/page.tsx
@@ -94,13 +94,13 @@ export default function CalendarPage() {
                     {userRole === 'fixer' && (
                         <div className="flex items-center ml-4">
                             <button
-                                className="w-60 bg-blue-500 text-white px-6 py-2 rounded hover:bg-blue-600 transition-colors"
+                                className="w-30 bg-blue-500 text-white px-2 py-2 rounded hover:bg-blue-600 transition-colors"
                                 onClick={handleOpenAvailabilityModal}
                             >
                                 Modificar Disponibilidad
                             </button>
                             <button className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600 transition-colors ml-2" onClick={openCancelModal}>
-                                Cancelar
+                                Cancelar Citas
                             </button>
                         </div>
                     )}
