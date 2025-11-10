066e162eafabae0008fef6fe98c6c335fe0c1746
Diego Perez <diego.fernando.perez.luna@gmail.com>
2025-11-10 17:22:58 -0400

    Add: generic modal

---
+++ src/shared/ui/ModalComponent.tsx
@@ -0,0 +1,45 @@
+interface ModalProps {
+    children: React.ReactNode;
+    isOpen?: boolean;
+    onClose: () => void;
+    Accept: () => void;
+}
+
+export const ModalComponent = ({
+    children,
+    isOpen,
+    onClose,
+    Accept,
+}: ModalProps) => {
+    if (!isOpen) return null;
+
+    return (
+        <div
+            className="bg-black bg-opacity-50 fixed inset-0 flex items-center justify-center z-50 w-full h-full"
+            role="dialog"
+            aria-modal
+            onClick={
+                onClose ||
+                (() => {
+                    isOpen = false;
+                })
+            }
+        >
+            <div className="flex flex-col gap-6 relative bg-white p-6 rounded-lg shadow-lg" onClick={(e) => e.stopPropagation()}>
+                <button
+                    className="absolute top-2 right-2 text-gray-600 hover:text-gray-800"
+                    onClick={
+                        onClose ||
+                        (() => {
+                            isOpen = false;
+                        })
+                    }
+                >
+                    x
+                </button>
+                {children}
+                <button onClick={Accept}>Accept</button>
+            </div>
+        </div>
+    );
+};
