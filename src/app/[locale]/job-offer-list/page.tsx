b232f4414b2f44f502f8b63030497bc185b64225
CrhistoferVera <cristofervera110@gmail.com>
2025-11-10 02:15:43 -0400

    add the dynamic routes using  [locale]

--- src/app/[locale]/job-offer-list/page.tsx
+++ src/app/[locale]/job-offer-list/page.tsx
@@ -1,19 +1,19 @@
 "use client"
 
 import { useState, useEffect, useMemo } from "react"
-import { mockJobOffers } from "../lib/mock-data"
-import { JobOffer } from "../lib/mock-data"
+import { mockJobOffers } from "../../lib/mock-data"
+import { JobOffer } from "../../lib/mock-data"
 import { JobOfferCard } from "@/Components/Job-offers/Job-offer-card"
 import { JobOfferModal } from "@/Components/Job-offers/Job-offer-modal"
 import { MapView } from "@/Components/Job-offers/maps/MapView"
 import { SearchHeader } from "@/Components/SearchHeader"
 import { FiltersPanel } from "@/Components/FiltersPanel"
-import { useAppSelector } from "../redux/hooks"
+import { useAppSelector } from "../../redux/hooks"
 import {
   selectSearchQuery,
   selectSelectedCities,
   selectSelectedJobTypes,
-} from "../redux/slice/filterSlice"
+} from "../../redux/slice/filterSlice"
 import { ArrowLeft, Briefcase, Map, List, ChevronLeft, ChevronRight } from "lucide-react"
 import Link from "next/link"
 
