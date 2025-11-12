38e5a6356ebfde9319fc8284c1f56ab687c999a8
alex-opt-21 <202010391@est.umss.edu>
2025-11-12 01:51:42 -0400

    feat: inicio rama integracion

--- src/app/controlC/HU1/FotoPerfil/layout.tsx
+++
@@ -1,9 +0,0 @@
-import React, { ReactNode } from "react";
-
-export default function Layout({ children }: { children: ReactNode }) {
-  return (
-    <div className="font-sans text-gray-900">
-      {children}
-    </div>
-  );
-}
