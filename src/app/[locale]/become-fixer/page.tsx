5bfa931a848adab277a53846f9fa00b9dec4f338
Adriana Lizbeth <202300665@est.umss.edu>
2025-11-13 07:15:58 -0400

    add an item to the dictionary spain

--- src/app/[locale]/become-fixer/page.tsx
+++ src/app/[locale]/become-fixer/page.tsx
@@ -4,12 +4,15 @@ import { useState } from 'react';
 import FixerRegisterForm from '@/Components/fixer/Fixer-register-form';
 import { FixerEnableWizard } from '@/Components/fixer/Filter-eneable-wizard';
 import { Navbar } from '@/Components/Shared/Navbar';
+import { useTranslations } from 'next-intl';
+import { title } from 'process';
 
 const defaultFormValues = {
   name: 'Freddy Amin Zapata',
   email: 'zapata@example.com',
   phone: '+591 68546043',
 };
+
 
 type RequesterUser = {
   id: string;
@@ -20,6 +23,7 @@ type RequesterUser = {
 };
 
 export default function BecomeFixerPage() {
+  const t = useTranslations("becomeFixer");
   const [requester, setRequester] = useState<RequesterUser | null>(null);
 
   return (
@@ -27,15 +31,13 @@ export default function BecomeFixerPage() {
       <Navbar />
       <div className="container mx-auto max-w-4xl p-4">
         <header className="mb-6 text-center">
-          <h1 className="text-2xl font-bold">Conviertete en un Fixer</h1>
-          <p className="text-sm text-gray-500">
-            Completa tu registro y habilita tu cuenta como FIXER
-          </p>
+          <h1 className="text-2xl font-bold">{t("title")}</h1>
+          <p className="text-sm text-gray-500">{t("description")}</p>
         </header>
 
         <section className="space-y-6">
           <div className="neon-border glass-panel rounded-2xl border border-gray-200 p-4 shadow-sm animate-slide-up">
-            <h2 className="mb-3 text-center text-lg font-semibold">Datos iniciales</h2>
+            <h2 className="mb-3 text-center text-lg font-semibold">{t("InputData")}</h2>
             <FixerRegisterForm
               defaultValues={defaultFormValues}
               onSubmit={(data) => {
@@ -49,7 +51,7 @@ export default function BecomeFixerPage() {
                   role: 'requester',
                 });
               }}
-              submitButtonText="Continuar"
+              submitButtonText={t("button1")}
             />
           </div>
 
