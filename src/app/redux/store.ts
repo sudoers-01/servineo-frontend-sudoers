e533f6c40e2c200018862f24f5e2029b4ff9c196
Antonfort <erick6suarez@gmail.com>
2025-11-19 20:43:37 -0400

    graphics

--- src/app/redux/store.ts
+++ src/app/redux/store.ts
@@ -14,7 +14,6 @@ export const store = configureStore({
     fixer: fixerReducer,
     filters: filterReducer,
     jobOffers: jobOffersReducer,
-    fixersByJob: fixersByJobReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware()
