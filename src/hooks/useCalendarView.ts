71712273547b22deb4cd37734cc9e6a56b74d8eb
Freddy Amin Zapata <zapatamorato@gmail.com>
2025-12-01 06:25:12 -0300

    dadg

--- src/hooks/useCalendarView.ts
+++ src/hooks/useCalendarView.ts
@@ -1,13 +1,3 @@
-<<<<<<< HEAD
-import { useState } from 'react';
-export default function useCalendarView() {
-  const [view, setView] = useState<'month' | 'week' | 'day'>('month');
-  const handleMonthView = () => setView('month');
-  const handleWeekView = () => setView('week');
-  const handleDayView = () => setView('day');
-
-  return { view, handleMonthView, handleWeekView, handleDayView };
-=======
 
 import { useState } from "react";
 export default function useCalendarView() {
@@ -18,5 +8,4 @@ export default function useCalendarView() {
 
     return { view, handleMonthView, handleWeekView, handleDayView };
 
->>>>>>> dev
 }
