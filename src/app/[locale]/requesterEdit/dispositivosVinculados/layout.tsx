8860f5c4a15a60d752c4bd6758e20f73bd515fa5
unknown <xavier10riv@gmail.com>
2025-12-01 23:23:12 -0400

    feat(dev):Corregir bugs

--- src/app/[locale]/requesterEdit/dispositivosVinculados/layout.tsx
+++ src/app/[locale]/requesterEdit/dispositivosVinculados/layout.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { AuthProvider } from '../../../lib/hooks/usoAutentificacion';
+import { AuthProvider } from '../../../../Components/requester/auth/usoAutentificacion';
 
 export default function HU7Layout({ children }: { children: React.ReactNode }) {
   return <AuthProvider>{children}</AuthProvider>;
