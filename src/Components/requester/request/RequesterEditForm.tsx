1e597463ff5f1758f2b2d53a04b2dde783f20d92
Victor Avalos(LuisDEG) <avalosvictor7@gmail.com>
2025-12-06 00:34:20 -0400

    style: formato automático aplicado (Prettier) y validación de build exitosa

--- src/Components/requester/request/RequesterEditForm.tsx
+++ src/Components/requester/request/RequesterEditForm.tsx
@@ -75,9 +75,11 @@ export default function RequesterEditForm() {
       import('leaflet').then((L) => {
         delete (L.Icon.Default.prototype as unknown as { _getIconUrl?: string })._getIconUrl;
         L.Icon.Default.mergeOptions({
-          iconRetinaUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png',
+          iconRetinaUrl:
+            'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png',
           iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png',
-          shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
+          shadowUrl:
+            'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
         });
       });
     }
